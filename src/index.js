50c4a6466153dad404cd539643020b1c74143fe4
Leila <soltani.leil@gmail.com>
2024-10-23 16:30:09 -0700

    Update index.js
    updated function:
    1- chooseHole

--- src/index.js
+++ src/index.js
@@ -44,7 +44,7 @@ function setDelay(difficulty) {
   if (difficulty === "easy") {
     return 1500;
   }
-  
+
   else if (difficulty === "normal") {
     return 1000;
   }
@@ -75,7 +75,13 @@ function setDelay(difficulty) {
  */
 function chooseHole(holes) {
   // TODO: Write your code here.
-
+  const index = randomInteger(0, 8);
+	const hole = holes[index];
+	if (hole === lastHole) {
+		return chooseHole(holes);
+	}
+	lastHole = hole;
+	return hole;
 }
 
 /**
