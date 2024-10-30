f1b6f7b722516487401d9cfe3d0461f0e014c328
Leila <soltani.leil@gmail.com>
2024-10-29 18:55:21 -0700

    Update index.js
    final functions

--- src/index.js
+++ src/index.js
@@ -180,7 +180,8 @@ function toggleVisibility(hole){
 */
 function updateScore() {
   // TODO: Write your code here
-
+  points += 1;
+	score.textContent = points;
   return points;
 }
 
@@ -193,8 +194,8 @@ function updateScore() {
 */
 function clearScore() {
   // TODO: Write your code here
-  // points = 0;
-  // score.textContent = points;
+  points = 0;
+  score.textContent = points;
   return points;
 }
 
@@ -204,6 +205,10 @@ function clearScore() {
 *
 */
 function updateTimer() {
+  if (time > 0) {
+		time -= 1;
+		timerDisplay.textContent = time;
+	}
   // TODO: Write your code here.
   // hint: this code is provided to you in the instructions.
   
@@ -218,7 +223,7 @@ function updateTimer() {
 */
 function startTimer() {
   // TODO: Write your code here
-  // timer = setInterval(updateTimer, 1000);
+  timer = setInterval(updateTimer, 1000);
   return timer;
 }
 
@@ -232,7 +237,7 @@ function startTimer() {
 */
 function whack(event) {
   // TODO: Write your code here.
-  // call updateScore()
+  updateScore();
   return points;
 }
 
@@ -243,6 +248,7 @@ function whack(event) {
 */
 function setEventListeners(){
   // TODO: Write your code here
+  moles.forEach((moles) => moles.addEventListener("click", whack));
 
   return moles;
 }
@@ -277,8 +283,11 @@ function stopGame(){
 *
 */
 function startGame(){
-  //setDuration(10);
-  //showUp();
+  setDuration(10);
+	showUp();
+	clearScore();
+	setEventListeners();
+	startTimer();
   return "game started";
 }
 
